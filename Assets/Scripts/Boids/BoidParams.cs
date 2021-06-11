222472bbda638361d0ec04d8fd58981e843a03a8
André Antonitsch <andre.antonitsch@acad.pucrs.br>
2021-06-11 20:45:59 -0300

    Buggy Boids

---
+++ Assets/Scripts/Boids/BoidParams.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidParams : MonoBehaviour
+{
+    public float separations;
+    public float alignment;
+    public float cohesion;
+    public float LOS;
+    public BoidTeam team;
+}
