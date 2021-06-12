fa009cf80785e67a0e608f8261dca745c2ce59bf
Diogo45 <diogohartmann45@gmail.com>
2021-06-12 18:35:00 -0300

    color reset

--- Assets/Scripts/Utility/ApplyPostProcessing.cs
+++ Assets/Scripts/Utility/ApplyPostProcessing.cs
@@ -7,6 +7,13 @@ public class ApplyPostProcessing : MonoBehaviour
 
     public Material PostProcessingMaterial;
 
+
+    private void Start()
+    {
+        var cam = GetComponent<Camera>();
+        cam.depthTextureMode = cam.depthTextureMode | DepthTextureMode.DepthNormals;
+    }
+
     protected virtual void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         Graphics.Blit(source, destination, PostProcessingMaterial);
