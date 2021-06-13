f01b659f786c6471eaac800ba59a9ae39cadf5a0
André Antonitsch <andre.antonitsch@acad.pucrs.br>
2021-06-12 23:05:10 -0300

    Capturable Object Logic

--- Assets/Scripts/City/CapturableObjective.cs
+++ Assets/Scripts/City/CapturableObjective.cs
@@ -8,16 +8,51 @@ public class CapturableObjective : MonoBehaviour
     public Material CaptureMat;
 
 
-    public delegate float OnChargeChange();
+    public delegate void OnChargeChange(float charge);
+    public delegate void OnChargeMaxed(CapturableObjective obj);
 
     public event OnChargeChange ChargeChanged;
+    public event OnChargeMaxed ChargeMaxed;
 
+    public bool Captured = false;
     public float CaptureCharge = 0;
     public float MaxCharge = 100.0f;
 
+    public float ChargeStrength = 1.01f;
+    public float ChargeCap = 8.0f;
+    public float ChargePow = 1.2f;
+    public float ChargeRate = 1.4f;
+    public float check_timer = 1.0f;
+
+    public HashSet<GameObject> neighbours;
+
+
+    public IEnumerator CheckCharge()
+    {
+        while (!Captured)
+        {
+            Charge();
+
+            yield return new WaitForSeconds(check_timer);
+        }
+    }
 
     public void Charge() 
     {
+        int quantity = neighbours.Count;
+
+        float charge = Mathf.Clamp(ChargeRate *  Mathf.Pow(ChargeStrength, ChargePow * quantity), 0f, ChargeCap);
+
+        CaptureCharge += charge;
+
+        ChargeChanged.Invoke(CaptureCharge);
+
+        if (CaptureCharge >= MaxCharge)
+        {
+            Captured = true;
+            ChargeMaxed.Invoke(this);
+        }
+
         UpdateMat();
     }
 
@@ -30,7 +65,25 @@ public class CapturableObjective : MonoBehaviour
     void Start()
     {
         CaptureMat = new Material(CaptureMat);
+
+        StartCoroutine(CheckCharge());
     }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Riot"))
+        {
+            neighbours.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Riot"))
+        {
+            neighbours.Remove(other.gameObject);
+        }
+    }
 
 }
